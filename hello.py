c02e7307bc8696368c023be2ef065c90274a95ec
obasiogbeyialu-cell <obasiogbeyialu@gmail.com>
2026-06-26 16:58:07 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-print("Hello Obasi!")
-print ("My name is Obasi")
-print ("I am a Medical Laboratory Scientist")
+# Write a program that uses a print function to say 'hello world' as shown in 'Desired Output'
+# the code below almost works
+
+print("hello world")
