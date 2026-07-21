65c46d93eaa1917a30f915a0a6708d0297e299c8
obasiogbeyialu-cell <obasiogbeyialu@gmail.com>
2026-07-21 10:49:20 +0100

    Added birth year calculation

--- patient_data_collector.py
+++ patient_data_collector.py
@@ -1,7 +1,9 @@
 name = input("What is your name? ")
 age = input("What is your age? ")
 job = input("What is your job? ")
+birth_year = 2026 - int(age)
 
 print("Name:", name)
 print("Age:", age)
 print("Job:", job)
+print("You were born in", birth_year)
